2d757d0deec26c4595473692fe5b69cfb3ed94c4
Âu Phước Vũ <gitlab7@novus-fintech.com>
2023-07-30 05:28:46 +0700

    Validate isTrue in localStorage before login

--- src/router.js
+++ src/router.js
@@ -1,11 +1,18 @@
 import React from "react";
-import {Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import MyRecord from "./pages/Record/MyRecord";
 import TopMyPage from "./pages/Home/TopMyPage";
 import Login from "./pages/Auth/Login/Login";
 import MainLayout from "./layouts/MainLayout";
 import AuthLayout from "./layouts/AuthLayout";
 import Column from "./pages/Column/Column";
+
+// Utility function to check isLogin status
+const isUserLoggedIn = () => {
+  // Replace "your_isLogin_key" with the key you are using to store the login status in localStorage
+  const isLogin = localStorage.getItem("isLogin");
+  return isLogin === "true"; // Assuming you are storing "true" as a string in localStorage
+};
 
 const routers = [
   {
@@ -44,6 +51,15 @@ export const Router = () => {
       {routers.map((item) => {
         const { layout: Layout, element: Element } = item;
         let element = <Element />
+
+        // Check if the route requires authentication (MainLayout)
+        if (Layout === MainLayout) {
+          // If the user is not logged in, redirect them to the login page
+          if (!isUserLoggedIn()) {
+            element = <Navigate to="/login" replace={true} />;
+          }
+        }
+
         if (Layout) {
           element = (
             <Layout>
@@ -60,7 +76,8 @@ export const Router = () => {
           />
         );
       })}
-      <Route path="/" element={<Navigate to="/login" replace={true} />} />
+      {/* Redirect to /home if isLogin is true */}
+      {isUserLoggedIn() ? <Navigate to="/home" replace={true} /> : <Route path="/" element={<Navigate to="/login" replace={true} />} />}
     </Routes>
   );
 };
